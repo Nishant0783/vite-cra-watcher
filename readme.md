e2c1c9f0b0e56a9251bc24c558dd28e94fc5a9be
Nishant Paliwal <paliwalnishant2003@gmail.com>
2024-07-12 19:14:56 +0530

    add readme file

---
+++ readme.md
@@ -0,0 +1,33 @@
+# React Boilerplate Generator
+
+vite-cra-watcher is an npm library that watches for new files in your React application's `components` or `pages` directories and automatically adds boilerplate code to them. This ensures that all new components and pages have a consistent starting structure.
+
+## Features
+
+- Watches for new files in `components` and `pages` directories.
+- Supports `.jsx`, `.js`, `.tsx`, and `.ts` file extensions.
+- Automatically adds boilerplate code to new files.
+- Ensures all new components and pages follow a consistent structure.
+
+## Installation
+
+You can install the library using npm:
+
+```bash
+npm install vite-cra-watcher
+
+## Usage
+
+1. Add the script to package.json file
+
+"scripts": {
+  "start:watcher": "vite-cra-watcher"
+}
+
+2. Run the following command to start the watcher
+
+npm run start:watcher
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit a pull request or open an issue on GitHub.
--- util/createRequireDir.js
+++ util/createRequireDir.js
@@ -5,23 +5,23 @@ const srcDir = './src';
 
 // Create components directory
 export function createCompDir() {
-        fs.mkdir(path.join(srcDir, 'components'), { recursive: true }, (err) => {
-            if (err) {
-                throw err;
-            } else {
-                console.log("components directory created successfully")
-            }
-        })
+    fs.mkdir(path.join(srcDir, 'components'), { recursive: true }, (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("components directory created successfully")
+        }
+    })
 }
 
 // Create pages directory
 export function createPagesDir() {
-        fs.mkdir(path.join(srcDir, 'pages'), { recursive: true }, (err) => {
-            if (err) {
-                throw err;
-            } else {
-                console.log("pages directory created successfully")
-            }
-        })
+    fs.mkdir(path.join(srcDir, 'pages'), { recursive: true }, (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("pages directory created successfully")
+        }
+    })
 }
 
